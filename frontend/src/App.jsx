df0cc987a314d3daef7265951cacc4ea251b194e
shreeraj <shreerajjadhav19@gmail.com>
2025-01-27 09:58:42 +0530

    milestone8 completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import {LoginPage,SignupPage} from './Routes.jsx'
+import {LoginPage,SignupPage,Home} from './Routes.jsx'
 
 import './App.css';
 
@@ -9,6 +9,8 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage/>} />
+      <Route path="/" element={<Home/>}/>
+
     </Routes>
     </BrowserRouter>
  
