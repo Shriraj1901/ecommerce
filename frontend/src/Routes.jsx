f2882c89bdfe3aedb34b9e1a8fdc47ec439653bb
shreeraj <shreerajjadhav19@gmail.com>
2025-02-18 10:06:20 +0530

    milestone15 completed

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,6 +1,7 @@
-import LoginPage from './pages/Login'
-import SignupPage from './pages/Signup';
-import Home from './pages/Home';
-import CreateProduct from './pages/createProduct';
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import Home from "./pages/Home";
+import CreateProduct from "./pages/createProduct";
 import MyProducts from './pages/myProduct';
-export {LoginPage,SignupPage,Home,CreateProduct,MyProducts};
+import Cart from './pages//cart';
+export { LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart };
